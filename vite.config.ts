8c5831740a26eab7c04b7d7cdd3f19cc3d33c1c4
Diwakar-28 <diwakarn288@gmail.com>
2026-06-19 17:48:42 +0530

    Revert config to pure standard Vite defaults

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   build: {
-    outDir: 'build',
+    outDir: 'dist',
     emptyOutDir: true,
   }
 })
